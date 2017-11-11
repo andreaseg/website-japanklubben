f88859f96c54d91dcc3be10eca51675de752f084
Andreas Grønlien <andreas@gronlien.no>
2017-11-11 12:59:51 +0100

    lagt til innhold

---
+++ content.js
@@ -0,0 +1,53 @@
+var content = [
+{
+title: "Calligraphy",
+img: "/media/calligraphy.png",
+text: 
+`Traditionally, Japanese calligraphy consists of using a brush and ink. It is also possible to write by using just water instead of ink, so-called 水書き (mizugaki, lit. “water-writing”). The main difference between these two methods is that by using ink your writings will be permanent, whereas with water the writings will only be temporary as the piece of paper will eventually dry up and be ready for more writing. The latter method obviously gives you the possibility to test out your writing skills without the fear of “messing up” the piece of paper, although this should not be a problem at all given how much paper we have!
+
+Thursday 26th January was the third time that Japan Club Norway held an open session in Japanese calligraphy. In total we were ten people participating in the event. One of the fun aspects with these sessions is obviously to get to meet new people. These calligraphy sessions are meant to be a way for people who are interested in Japanese culture to gather together and socialise. This could obviously be said to be true for this day, with music, jokes and lots of conversing – all in all, it was a fun time!
+
+Japan Club Norway will hold cultural activites similar to this one every two weeks, so be on the lookout if this sounds interesting to you.`
+},
+{
+title: "Tea Ceremony",
+img: "/media/tea_ceremony.png",
+text: `
+Since the event was organized at an ordinary classroom, the organizers arrived in advance to prepare the room by moving chairs and tables and putting out the tea equipment. We didn’t have much funding for this event so we had to use what we had at home. In a proper tea-ceremony very special equipment is needed which is made just for the purpose of being used in the ceremony. For example we used an ordinary water cookers to boil the water, where we should have used a tetsubin (鉄瓶 – iron kettle) on a binkake (瓶掛 – stand for tetsubin), but the result is the same: hot water.
+
+All the movements in the making of tea in a tea-ceremony is done according to specific rules.
+All the movements in the making of tea in a tea-ceremony is done according to specific rules.
+
+On the flip side Kurt honored us with a beautiful kakejiku (掛け軸 – hanging scroll) which read hana yori dango (花より団子 – dango rather than flowers) which we hung behind the guest mat.We were invited to reflect on the meaning of this, and later it was revealed that Andreas had made dango-sticks which could have been of relevance. The dango was flavored elegantly with macha for green color and strawberry for red. First the bowl was passed along the line of three guests who were placed opposite the host (Andreas). The container was raised in an expression of thankfulness, a stick placed on a paper napkin and then passed to the next guest.The dango were very delicious but unfortunately some of the paper got stuck, so maybe a different type of paper would be good for future use.
+
+When drinking the tea, place the chawan in your left palm, steady with your right hand, keep your chin pulled in slightly with your back straight.
+When drinking the tea, place the chawan in your left palm, steady with your right hand, keep your chin pulled in slightly with your back straight.
+
+Then it was time for tea. Andreas had written a nice summary of the rules for the guests which was distributed, and read through. When drinking tea in such a ceremony almost all movements are to be according to special rules so that nothing is left to chance and the guests get the opportunity to concentrate fully on the now. If done correctly, and with much practice, one can even acheieve a meditative state while doing it. For us beginners it took some time to read through these rules and even then, since they were numerous they were soon forgotten. I think a possible improvement here is to, instead of reading through everything at once and trying to remember everything. Just focus on the first few beginning steps, do them in unison with the host making sure that everything is done correctly, then incorporate more steps until all steps are accounted for. Kind of how one would learn a kata in karate.
+
+Even though I had some problems remembering all the steps, this did not stop me from enjoying the tranquil atmosphere and delicious snacks and tea. The macha tea was nicely prepared by the host Andreas by the use of a traditional whisk and spoon. All in all it was a nice break from the hustle and bustle of everyday life to only concentrate on one thing: tea.
+
+The summary for rules of the tea-ceremony can be found [here]
+
+– Fredrik Nicolai Krohg
+`
+},
+{
+    title: "End of year",
+    img: "/media/end-of-year.png",
+    text: `
+    The end of the semester party was meant as a symbolic end of the semester where we celebrated our efforts in learning Japanese and another semester of events and development of Japan Club Norway.
+    
+    For this semester’s party we chose Amber-sushi as our venue since they could provide a private “samurai-room” which we ordered at no additional cost. The room was set apart from the main restaurant inside a doorway covered by a curtain. Inside was a dramatic portrayal of a samurai, a long table and some other decorations, perfect for such an occasion.
+    
+    We got special service when ordering the Party Mix
+    We got special service when ordering the Party Mix
+    
+    The evening started with some small talk between the early comers and when everyone had arrived, a group of people, me included, decided to ordered the appropriately named Party Mix set. This included 50 pieces of sushi that our waiter was so kind enough to change from mainly Phildelphia cheese maki to more nigiri and sashimi. Before eating I, as the organizer, opened with a welcome speech in Japanese where I thanked everyone for coming, talked a bit about the Genki course and finally thanked president Andreas for making Japan Club Norway possible. A symbolic present was given, and president Andreas then had his own speech where he thanked us in the board for the surprising present and talked a bit about the Japan Club Norway’s history and future.
+    
+    After consuming our delicious feast we held a small quiz which included diverse esoteric questions of trivia about Japan. It turned to be more difficult than anticipated such that the winning group (president Andreas and Stian) only got 4 correct answers out of 11 possible.
+    
+    Finally, a prize – consisting of mochi candy – was awarded to Kurt for most correct answers in the Kanji-tests of the Genki course with the impressive 87.70% correct of all possible Kanjis this semester, and 100% of all Kanji-tests he participated in. There were also a prize for the winners of the quiz and finally the membership-magazines were distributed to those who had paid for it.
+    `
+}
+];
