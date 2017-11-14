22929720d3a9cee7aa8ff5a18fcefb68188c1f5a
Andreas Grønlien <andreas@gronlien.no>
2017-11-14 21:08:55 +0100

    lagt til artikkel (fra magasin) om etikette

--- content.js
+++ content.js
@@ -126,5 +126,164 @@ The summary for rules of the tea-ceremony can be found <a href="./media/chadou_r
     </p>
     `
 
+},
+{
+    title: "Kanjietymology [Norsk]",
+    img: "./media/calligraphy_ei.jpg",
+    text: `
+    <p>
+    Blant <i>jouyoukanji</i><a href="#footnote-1">[1]</a> er det hele 2136 kanji. 
+    Alle disse kanjiene har en historie bak hvorfor de ser ut akkurat slik som de gjør. Kanji kan bestå av én eller flere deler. 
+    Disse delene kan være av piktografisk, ideografisk eller fonetisk sort. La oss begynne med å se på noen av de enkleste kanjiene.
+    </p>
+    <h2>
+    Piktrogrammer
+    </h2>
+    <p>
+    Et piktogram er et symbol som abstrakt skal vise en gjenstand. 山 er et klassisk eksempel, hvor de tre vertikale strekene skal symbolisere 
+    toppene på et fjell. Et annet eksempel er 日, som er en avbildning av solen.
+    </p>
+    <h2>
+    Ideogrammer
+    </h2>
+    <p>
+    Det er mange konsepter som ikke kan illustreres med et enkelt piktrogram. Ved slike tilfeller har man snarere måttet benytte seg av et ideogram. 
+    Ideogrammer er symboler som istedenfor illustrerer et abstrakt konsept som en tanke eller en handling. F.eks. er 下 og 上 henholdsvis 
+    tegnene for ned og opp, og 大 og 小 er henholdsvis tegnene for stor og liten. 大 illustrerer handlingen å strekke armer og ben for å gjøre 
+    seg så stor som mulig, mens 小 illustrerer det motsatte.
+    </p>
+    <h2>
+    Sammensatte kanji
+    </h2>
+    <p>
+    La oss nå se på kanjiene 持 og 時 (hhv. «å holde»  og «tid»). Merk at venstredelen av 持 aldri brukes som sin egen kanji. 
+    Venstredelen betyr «hånd» og kan ansees som en forenklet versjon av 手. Høyredelen av både 持 og 時 er 寺 som betyr «tempel». 
+    Originalbetydningen til 寺 var å holde eller å eie noe. 持 er dermed satt sammen av tegn for hånd og å holde noe, og kan forstås som å holde noe i 
+    hånden. 日 brukes ofte om dag, men kan også assosieres med det som har med tid eller dato å gjøre. 時 består da følgende av deler for «tid» og «å eie», 
+    noe som gir originalbetydningen av et kort tidsrom.
+    </p>
+    <h2>
+    Fonetisk komponent
+    </h2>
+    <p>
+    Mange kanji inneholder en fonetisk del. Hvis kanjien er horisontaldelt, er dette ofte høyredelen, 
+    som hverken har en piktografisk eller en ideografisk betydning. Denne delen gir oss i stedet en instruks 
+    om hvordan den originalt skulle leses på kinesisk. Hvis vi ser igjen på 寺, ser vi at den videre kan deles i to. 
+    Toppen er en forenkling av 止, og bunnen er 寸. I dette tilfellet har man valgt 止 fordi det gir lyden <i>shi</i>/<i>ji</i>
+    <a href="#footnote-2">[2]</a>. 寸 er et ideogram av en hånd som holder en gjenstand. V
+    i kan således se hvordan 持, 時 og 寺 alle har arvet onyomi<a href="#footnote-3">[3]</a> fra 止.
+    </p>
+
+    <p>
+    Skrevet av Andreas Grønlien for medlemsbladet 2016/2017
+    </p>
+
+
+    <p id="footnote-1">[1] 常用漢字: Kanji alle i Japan skal lære mens de går på skole</p>
+    <p id="footnote-2">[2] For fonetiske komponenter endrer ofte lyden seg litt.</p>
+    <p id="footnote-3">[3] 音読み: Kinesisk lesemåte.</p>
+    `
+},
+{
+    title: "Matetikette [Norsk]",
+    img: "./media/foodz.jpg",
+    text:`
+    <p>
+    Som i ethvert land finnes det mange uskrevne regler i Japan for hvordan man skal oppføre seg. Siden vår kultur
+    er såpass forskjellig fra den japanske, kan det være lurt å sette seg litt inn i hvordan ting gjøres over
+    på dette magiske arkipelet hvis man ønsker å unngå å tråkke i noen av de mest pinlige salatene. Siden det finnes altfor mange
+    (uskrevne) regler til å gå igjennom på én gang, begrenser vi oss i denne artikkelen til regler som har å gjøre med spisepinner 
+    og sushi.
+    </p>
+
+    <h2>Spisepinner</h2>
+    <p>
+    Spisepinner er for vestlige kanskje én av de første utfordringene man
+    blir konfrontert med ved kontakt med det asiatiske kjøkken. Ikke nok med at bare handlingen å transportere biter
+    med mat fra et fat til munnen kan være notorisk vanskelig
+    for oss som er vokst opp med kniv og gaffel, men i tillegg har japanere
+    en hel rekke abstrakte regler for sin bruk som du
+    helst ikke bør bryte, med mindre du ønsker å minne dine japanske verter om deres bestemors begravelse.
+    
+    Først tar vi noen regler som kanskje bør være åpenbare.
+    <ol>
+        <li> Ikke pek med spisepinnene <br />
+        – peking er stygt. </li>
+        <li> Ikke slikk matrester av spisepinnene – ikke alle er interesserte i å se på tungen din mens de spiser.</li>
+        <li> Ikke ta mat fra et felles bord med pinner du har hatt i munnen. Hvis du ikke har et ekstra par tilgjengelig som kan brukes til dette formålet, kan du snu pinnene og bruke den tykke siden til dette.</li>
+        <li> Noen ganger ønsker man å gni spisepinnene mot hverandre for å få vekk flis, men dette er frekt mot
+        restauranten, så prøv å unngå at noen ser deg, og aldri gjør det på fine restauranter der du får ordentlige
+        spisepinner – da trenger du det ikke.</li>
+    </ol>
+    Så noen litt mer esoteriske.
+    <ol>
+        <li>Legg aldri spisepinnene i kryss. Dette er en vanlig regel i nesten alle asiatiske land.</li>
+        <li>Ikke gjør noe som fører til kontakt mellom dine og andres spisepinner. F.eks. skal man aldri transportere mat fra spisepinnene dine direkte til spisepinnene til noen andre.</li>
+        <li>Ikke stikk spisepinnene ned i maten. Hvis man gjør dette er det sagt at det kan minne om hvordan røkelse er stukket ned i aske ved begravelser.</li>
+        <li>Ikke legg spisepinnene over skålen når du skal bruke hånden til noe annet. Dette er en veldig vanlig feil, selv blant japanere, men den korrekte måten er å legge dem ned på en liten spisepinnehviler. Hvis man ikke har en, kan man eventuelt brette en av papiremballasjen pinnene kom i, eller i nødstilfelle lene dem på skrå mot skålen.</li>
+        <li>Ikke sirkle med spisepinnnene over mat før du plukker den opp. Dette kalles <i>mayoibashi</i><a href="#footnote-1">[1]</a>, altså "spisepinner som har gått seg bort".</li>
+        <li>Spis litt ris mellom sideretter slik at du unngår å begå <i>utsuribashi</i><a href="#footnote-2">[2]</a>,
+        som betyr «vandrende/endrende spisepinner».</li>
+    </ol>
+    </p>
+    
+    
+
+    <figure style="width:40%">
+    <img src="./media/tohoku_sushi.jpg" style="width:100%">
+    <figcaption>Ekte Tohoku-sushi.</figcaption>
+    </figure>
+
+    <h2>På Sushirestaurant</h2>
+    
+    <p>
+    Sushiens popularitet har eksplodert til et nivå internasjonalt som ikke engang de fleste japanere er klar
+    over. Dermed er det et absolutt "must" å spise sushi på en ordentlig japansk sushirestaurant når man drar til Japan.
+    For å fortære
+    et slikt fabelaktig måltid uten at <i>itamae</i><a href="footnote-3">[3]</a>
+    igjen må minne seg selv om å ikke bli sint på disse <i>gaijin</i>-ene, bør man huske på dette:
+    <ol>
+        <li> Før du skal spise, si: <i>itadakimasu</i> som bokstavlig talt betyr noe slikt som "jeg mottar ærbødigst". </li>
+        <li> Ikke la det bli noe igjen. Ikke bare er det høflig, men japanere blir ofte fortalt av sine foreldre
+        at det bor syv guder i hvert riskorn, og skulle man la ett gå til spille, ville disse gudene forbanne dem.</li>
+        <li> Ikke legg wasabi i soyasausen. Er du på en ordentlig restaurant skal det være "riktig mengde "wasabi allerede lagt under fisken når du spiser <i>nigirizushi</i><a href="footnote-4">[4]</a>. Hvis
+        du ønsker mer, legg heller wasabien direkte oppå toppingen.</li>
+        <li> Hvis du ønsker å dyppe i soyasaus, dypp fisken og ikke risen. Risen har en tendens til å trekke til
+        seg for mye saus og desintegrere. Spiser du nigiri med spisepinner,
+        er trikset her å først vippe biten på siden slik at du får tak over og under. Da blir det lettere å dyppe
+        bare toppingen. Hvis du har en bit slik som <i>gunkan</i>-sushi, der toppingen
+        vil falle ut hvis du holder biten opp ned, dypp litt ingefær i soyasausen og bruk dette til å lett smøre
+        litt soyasaus på toppen av biten.</li>
+        <li> Når du spiser en bit nigiri-zushi, legg den inn i munnen med toppingen ned mot tungen. Dette er for
+        at smakingen av toppingen skal komme best mulig frem.</li>
+        <li> Ikke del opp sushien. Spiser du nigiri, er det meningen at hele biten skal spises på én gang, ellers er det lett at den går i stykker. Sushi var opprinnelig en fast food solgt til travle businessmenn, og derav
+        har det blitt vanlig at bitene skal spises relativt raskt. Dette er også for at bitene skal ha riktig
+        temperatur og ikke kjøles ned for mye når de blir spist.</li>
+        <li> Nigiri-sushi er en av de få japanske rettene man kan spise med hendene. Ta da tak med tommel på den
+        ene siden, pekefinger på kortsiden og lange og ringefinger på den andre langsiden for å løfte den opp.
+        Bruk pekefingeren til å flippe biten rundt for dypping og for at toppingen skal komme ned mot tungen når
+        du spiser den. Hvis du ønsker å prøve denne håndmetoden, husk å på forhånd tørke av hendene på det lille (ofte oppvarmede) hvite håndkledet kalt <i>oshibori</i><a href="footnote-5">[5]</a> som følger med
+        (gitt at du er på ett ordentlig sted). Ikke bruk dette håndkledet til å tørke andre ting, f.eks. ansiktet. </li>
+        <li> Unngå bruk av <i>tesara</i><a href="footnote-6">[6]</a>, bokstavelig "håndtallerken": Det å holde
+        hånden under mat for å unngå at det faller.</li>
+        <li> Unngå å bøye deg over suppen for mye hvis du får servert miso-suppe. Før heller skålen opp mot
+        ansiktet enn ansiktet ned mot skålen – tenk at du er en stolt samurai, ikke en tørst hund!</li>
+        <li> Avslutt måltidet med å si: <i>gochisosamadeshita</i> som betyr "det var ett godt måltid".</li>
+    </ol>
+    </p>
+
+    <p>
+        Skrevet av Fredrik Nicolai Krogh for medlemsbladet 2016/2017
+    </p>
+
+    <br />
+
+    <p id="footnote-1">[1] <ruby>迷い箸 <rt> まよいばし </rt></ruby></p>
+    <p id="footnote-2">[2] <ruby>移り箸 <rt> うつりばし </rt></ruby></p>
+    <p id="footnote-3">[3] <ruby>板前 <rt> いたまえ </rt></ruby> Bokstavelig person foran skjærefjølen</p>
+    <p id="footnote-4">[4] <ruby>握り寿司 <rt> にぎりずし </rt></ruby></p>
+    <p id="footnote-5">[5] お<ruby>絞 <rt> しぼ </rt></ruby></p>
+    <p id="footnote-6">[6] <ruby>手皿 <rt> てさら </rt></ruby></p>
+    `
 }
 ];
